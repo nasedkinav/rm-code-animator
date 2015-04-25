78f08b6e44a9dd783aaf62e33d3d43234c72c085
NasedkinAV <nasedkinav@eruditor-group.com>
2015-04-25 20:56:17 +0300

    channel update

---
+++ src/model/TransmitChannel.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public class TransmitChannel {
+
+    public static void stream(BitMatrix data, int error) {
+        Random generator = new Random();
+
+        for (int i = 0; i < data.getRowNumber(); i++) {
+            List<Boolean> row = data.getRow(i);
+            int currentError = generator.nextInt(error + 1);
+            Set<Integer> errorIndex = new HashSet<Integer>();
+            while (errorIndex.size() < currentError) {
+                int currentIndex = generator.nextInt(row.size());
+                if (!errorIndex.contains(currentIndex)) {
+                    errorIndex.add(currentIndex);
+                }
+            }
+            for (Integer index : errorIndex) {
+                row.set(index, BinaryFiniteField.invert(row.get(index)));
+            }
+        }
+    }
+}
